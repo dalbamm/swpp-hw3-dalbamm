5d5a859fd0cfa6c469f41a903671eba730303d04
dalbamm <oscar.yoon13@gmail.com>
2018-10-28 15:10:12 +0900

    Add Article class

--- django/blog/models.py
+++ django/blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Article(models.Model):
+	title = models.CharField(max_length=120)
+	content = models.TextField(max_length=120)
+	author = models.ForeignKey(
+			User,
+			on_delete=models.CASCADE,
+			related_name='author_set',
+	)

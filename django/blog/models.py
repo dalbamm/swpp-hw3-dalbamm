c9ff2b429b34b9ace9cbbcd9eb832f65279471a8
dalbamm <oscar.yoon13@gmail.com>
2018-10-28 15:31:51 +0900

    Add comment class and test done

--- django/blog/models.py
+++ django/blog/models.py
@@ -4,9 +4,26 @@ from django.contrib.auth.models import User
 
 class Article(models.Model):
 	title = models.CharField(max_length=120)
-	content = models.TextField(max_length=120)
+	content = models.TextField(max_length=500)
 	author = models.ForeignKey(
 			User,
 			on_delete=models.CASCADE,
 			related_name='author_set',
 	)
+	def __str__(self):
+		return "title: {}/ content: {}".format(self.title, self.content)
+
+class Comment(models.Model):
+	article = models.ForeignKey(
+		Article,
+		on_delete=models.CASCADE,
+		related_name='article_set'
+	)
+	content = models.TextField(max_length=500)
+	author = models.ForeignKey(
+			User,
+			on_delete=models.CASCADE,
+			related_name='comment_author_set',
+	)
+	def __str__(self):
+		return "content: {}".format(self.content)

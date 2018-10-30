d3ee5ea4978a1d089bcea6170c94cdc41446d60b
dalbamm <oscar.yoon13@gmail.com>
2018-10-30 19:55:15 +0900

    Article, Comment requests done

--- django/blog/urls.py
+++ django/blog/urls.py
@@ -2,8 +2,12 @@ from django.urls import path
 from blog import views
 
 urlpatterns = [
-    path('signup', views.signup, name='signup'),
-    path('token', views.token, name='token'),
-    path('signin', views.signin, name='signin'),
-    
+    path('signup/', views.signup, name='signup'),
+    path('token/', views.token, name='token'),
+    path('signin/', views.signin, name='signin'),
+    path('signout/', views.signout, name='signout'),
+    path('article/', views.article, name='article'),
+    path('article/<int:article_id>/', views.article_id, name='article_id'),
+    path('article/<int:article_id>/comment/', views.comment, name='comment'),
+    path('comment/<int:comment_id>/', views.comment_id, name='comment_id'),
 ]
